7c9b1223e469b0529e0d79a423673697962d124d
Sohail Aziz <sohail.aziz05@gmail.com>
2025-08-22 17:38:47 +0500

    adds test dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,9 @@ dependencies {
     // Kotlin serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+
 
 
 }
