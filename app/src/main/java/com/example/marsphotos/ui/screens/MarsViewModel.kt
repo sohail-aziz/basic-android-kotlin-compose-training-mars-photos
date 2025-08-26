4565e1f75e6402acf134982685ee600a4d66cdae
Sohail Aziz <sohail.aziz05@gmail.com>
2025-08-27 00:49:39 +0500

    uses Coil to display mars photo

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -35,7 +35,7 @@ import java.io.IOException
  * UI state for the Home screen
  */
 sealed interface MarsUiState {
-    data class Success(val photos: String) : MarsUiState
+    data class Success(val photos: MarsPhoto) : MarsUiState
     object Error : MarsUiState
     object Loading : MarsUiState
 }
@@ -60,9 +60,9 @@ class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : Vi
         viewModelScope.launch {
             marsUiState = MarsUiState.Loading
             marsUiState = try {
-                val listResult = marsPhotosRepository.getMarsPhotos()
+                val result = marsPhotosRepository.getMarsPhotos()[0]
                 MarsUiState.Success(
-                    "Success: ${listResult.size} Mars photos retrieved"
+                    result
                 )
             } catch (e: IOException) {
                 MarsUiState.Error
